e3928bcc4295c98b7ddce72d86aaf6de4900a20c
Ola Tuvesson <ola@iron.clickworkorange.net>
2024-01-05 22:51:14 +0000

    Butterfingers.

--- AVR/avr_pwm_rtu.h
+++ AVR/avr_pwm_rtu.h
@@ -7,7 +7,7 @@
 
 #define CHANNELS 4
 #define LEVELS 4
-#define REGCOUNT 50
+#define REGCOUNT 51
 // size in bits of max register value - set to zero for read only registers
 static uint8_t const REGSIZE[] = {2,2,2,2,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,1,3,1,3,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,8,3,2,1,0,0,1};
 #define read_register_array(address,value_p,length) eeprom_read_block ((void *)value_p, (const void *)address, length)
